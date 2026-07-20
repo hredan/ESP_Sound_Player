91bfbdab8cbcf57cbcb5d4d66907798396e62271
hredan <hredan@sourcecode3d.de>
2026-07-20 15:49:14 +0200

    Modified Squeezbox is running with an ESP32 super mini c3

--- handleAudio.ino
+++ handleAudio.ino
@@ -1,11 +1,46 @@
 
 #include "handleAudio.h"
 
+ReactiveAudioOutputI2S::ReactiveAudioOutputI2S() : _level(0.0f)
+{
+}
+
+bool ReactiveAudioOutputI2S::ConsumeSample(int16_t sample[2])
+{
+    int32_t left = sample[0];
+    int32_t right = sample[1];
+    float peak = abs(left);
+    if (abs(right) > peak)
+    {
+        peak = abs(right);
+    }
+    float normalized = peak / 32768.0f;
+
+    // Fast attack and slower release for stable visualization.
+    const float attack = 0.25f;
+    const float release = 0.04f;
+    if (normalized > _level)
+    {
+        _level += (normalized - _level) * attack;
+    }
+    else
+    {
+        _level += (normalized - _level) * release;
+    }
+
+    return AudioOutputI2S::ConsumeSample(sample);
+}
+
+float ReactiveAudioOutputI2S::getLevel() const
+{
+    return _level;
+}
+
 #include <SD.h>
 #include <ctype.h>
 
 namespace {
-const char *AUDIO_PINOUT_CONFIG_FILE_NAME = "audioPinoutConfig.json";
+const char *AUDIO_PINOUT_CONFIG_FILE_NAME = "/audioPinoutConfig.json";
 
 bool tryReadIntFromJson(const String &json, const char *key, int &value)
 {
