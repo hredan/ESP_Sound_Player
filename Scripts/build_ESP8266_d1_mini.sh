10418637e581d4ed23afc267723657d73a4014d4
hredan <hredan@sleepuino.de>
2026-07-22 09:22:19 +0200

    test workflow

--- Scripts/build_ESP8266_d1_mini.sh
+++ Scripts/build_ESP8266_d1_mini.sh
@@ -9,8 +9,7 @@ export INPUT_CORE="esp8266"
 export INPUT_BOARD="d1_mini"
 export INPUT_CORE_VERSION=""
 export INPUT_CPU_F="80"
-export INPUT_LIBS="ESP8266Audio"
-export INPUT_FLASH="4M1M"
+# export INPUT_FLASH="4M1M"
 export INPUT_LIBS="ESP8266Audio,'Adafruit NeoPixel'"
 
 URL=$(git remote get-url origin)
