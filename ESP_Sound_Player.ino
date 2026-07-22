736f6cf8367c44629c01362449b2acf9803f22a2
hredan <hredan@sleepuino.de>
2026-07-22 10:53:51 +0200

    workflow die letzte?

--- ESP_Sound_Player.ino
+++ ESP_Sound_Player.ino
@@ -3,10 +3,10 @@
 #else
 #include <ESP8266WiFi.h>  // https://github.com/esp8266/Arduino
 #endif
-#include "handle_audio.h"
-#include "handle_esp_config.h"
-#include "handle_led_ring.h"
-#include "handle_webpage.h"
+#include "./handle_audio.h"
+#include "./handle_esp_config.h"
+#include "./handle_led_ring.h"
+#include "./handle_webpage.h"
 #include "LittleFS.h"
 #include <SD.h>
 #include "Config.h"
